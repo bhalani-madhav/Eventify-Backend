9ef06bbfd9c0516c61cb07b655260a77f663e8a9
bhalani-madhav <madhav.bhalani@thetatechnolabs.com>
2025-05-13 14:43:48 +0530

    added reminders model

---
+++ models/reminders.js
@@ -0,0 +1,55 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Reminders extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Reminders.init({
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: sequelize.literal('gen_random_uuid()'),
+      primaryKey: true,
+      unique: true,
+      autoIncrement: false,
+    },
+    title:{
+      type: DataTypes.STRING(100),
+      allowNull: false,  
+    
+    },
+    description:{
+        type: DataTypes.STRING(500),
+        allowNull: false
+    },
+    date:{
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        validate: {
+            isDate: true
+        }
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+  }, {
+    sequelize,
+    modelName: 'Reminders',
+    tableName: 'reminders',
+    underscored: true,
+  });
+  return Reminders;
+};
