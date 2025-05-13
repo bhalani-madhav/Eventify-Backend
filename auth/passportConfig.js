a8f44ce0b89378c47589013846b66012ce6626c7
bhalani-madhav <madhav.bhalani@thetatechnolabs.com>
2025-05-13 17:41:18 +0530

    added authentication using passport-jwt and added auth middleware in logout route and created a sample secret route to test authentication

---
+++ auth/passportConfig.js
@@ -0,0 +1,35 @@
+const passport = require("passport");
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { Users } = require("../models");
+
+const cookieExtractor = (req) => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies.jwt;
+  }
+  console.log('Token: ', token);
+  return token;
+};
+
+module.exports.initializePassport = (passport) => {
+  
+  passport.use(
+    new JWTStrategy(
+      {
+        jwtFromRequest: cookieExtractor,
+        secretOrKey: "santaClause90*32@@",
+      },
+      async (jwtPayload, done) => {
+        const user = await Users.findByPk(jwtPayload.sub);
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      }
+    )
+  );
+};
